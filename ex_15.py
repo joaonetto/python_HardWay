071add658a4f602e23d97d5934180722bb50c2d1
actargroup <joao.netto@actar.com.br>
2017-12-27 10:19:35 -0200

    Tratando arquivos

---
+++ ex_15.py
@@ -0,0 +1,29 @@
+import sys
+import os.path
+from sys import argv
+
+if len(sys.argv) < 2:
+    print(f"Para este escript é necessário 2 argumentos, mas você passou apenas {len(sys.argv)}")
+    exit()
+
+script, filename = argv
+
+if os.path.isfile(filename) == False:
+    print(f"O arquivo {filename} não existe.\nVerifique o nome e execute novamente")
+    exit()
+
+txt = open(filename)
+
+print(f"Here's your file {filename}:")
+print(txt.read())
+
+print("Type the filename again:")
+file_again = input("> ")
+
+if os.path.isfile(file_again) == False:
+    print(f"Você passou um novo arquivo {file_again} que não existe.\nVerifique o nome e execute novamente")
+    exit()
+
+txt_again = open(file_again)
+
+print(txt_again.read())
