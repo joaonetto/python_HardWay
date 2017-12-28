f815125d9d643713482ea1f9feb2da5569aa9269
actargroup <joao.netto@actar.com.br>
2017-12-28 14:06:37 -0200

    Comentario para UTF-16

--- ex_23.py
+++ ex_23.py
@@ -21,3 +21,7 @@ def print_line(line, encoding, errors):
 languages = open("ex_23_languages.txt", encoding="utf-8")
 
 main(languages, input_encoding, error)
+
+# Exemplo de utilizar o print com encode/decode
+# >>> print(b"some utf-16 encoded bytestring \xc3\x90\xc2\xa2\xc3\x90\xc2\xbe\xc3\x92\xc2\xb7\xc3\x90\xc2\xb8\xc3\x90\xc2\xba\xc3\x93\xc2\xa3".decode('utf-16'))
+# 潳敭甠晴㠭攠据摯摥戠瑹獥牴湩⁧郃ꋂ郃뻂鋃럂郃룂郃뫂鏃ꏂ
