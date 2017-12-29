4db903a56311e14319bb499d74fcf0ae85cdb081
actargroup <joao.netto@actar.com.br>
2017-12-29 07:16:40 -0200

    Ajuste nas funções

--- ex_25_functions.py
+++ ex_25_functions.py
@@ -11,13 +11,11 @@ def print_first_word(words):
     """Prints the first word after popping it off."""
     word = words.pop(0)
     return word
-    #print(word)
 
 def print_last_word(words):
     """Prints the last word after popping off."""
     word = words.pop(-1)
     return word
-    #print(word)
 
 def sort_sentence(sentence):
     """Takes in a full sentence and return the sorted words."""
