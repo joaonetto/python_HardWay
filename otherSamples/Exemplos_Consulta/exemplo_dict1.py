76e1c163c030ca4c0ff0434b7ec5e099a882f10c
actargroup <joao.netto@actar.com.br>
2018-03-23 14:09:15 -0300

    Exemplo Dicionario

--- otherSamples/Exemplos_Consulta/exemplo_dict1.py
+++ otherSamples/Exemplos_Consulta/exemplo_dict1.py
@@ -2,7 +2,20 @@ import json, os
 
 os.system('clear')
 
-a = '{"ge-0/0/0": {"oper": "up", "admin": "up", "description": "Interface de UpLink - Firewall Juniper SRX 240 - ge-0/0/15", "mtu": 9216, "link_mode": null, "speed": "Auto", "macaddr": "3c:61:04:67:6c:03", "flapped": "2017-12-22 18:33:18 UTC (1w3d 11:09 ago)"}}'
+a = '{"ge-0/0/0":
+        {
+            "oper": "up",
+            "admin": "up",
+            "description": "Interface de UpLink - Firewall Juniper SRX 240 - ge-0/0/15",
+            "mtu": 9216,
+            "link_mode": null,
+            "speed": "Auto",
+            "macaddr": "3c:61:04:67:6c:03",
+            "flapped": "2017-12-22 18:33:18 UTC (1w3d 11:09 ago)"
+        }
+    }'
+
+
 b = '{"ge-0/0/0": {"rx_bytes": 8145728, "rx_packets": 127277, "tx_bytes": 42984564, "tx_packets": 532676, "rx_err_input": 0, "rx_err_drops": 0}}'
 c = '{"ge-0/0/0": {"rx_bytes": 8145728, "rx_packets": 127277, "tx_bytes": 42984628, "tx_packets": 532677, "rx_err_input": 0, "rx_err_drops": 0, "rx_err_frame": 0, "rx_err_runts": 0, "rx_err_discards": 0, "rx_err_l3-incompletes": 0, "rx_err_l2-channel": 0, "rx_err_l2-mismatch": 0, "rx_err_fifo": 0, "rx_err_resource": 0, "tx_err_carrier-transitions": 33, "tx_err_output": 0, "tx_err_collisions": 0, "tx_err_drops": 0, "tx_err_aged": 0, "tx_err_mtu": 0, "tx_err_hs-crc": 0, "tx_err_fifo": 0, "tx_err_resource": 0}}'
 
